f4d778a4a3aeac74cfb79efc88028dcebacebc7a
yamato0331 <yamatotunasima1@gmail.com>
2019-06-05 10:37:08 +0900

    seed追加

--- sns_api/db/seeds.rb
+++ sns_api/db/seeds.rb
@@ -5,3 +5,47 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!([
+    {
+        name: "user1",
+        password: "0001"
+    }, {
+         name: "user2",
+        password: "0002"
+    }
+])
+
+Follow.create!([
+    {
+        user_id: 1,
+        followed_id: 2
+    }, {
+        user_id: 2,
+        followed_id: 1
+    }
+])
+
+SentMessage.create!([
+    {
+        user_id: 1,
+        sent_user_id: 2,
+        message: "user1dayo"
+    }, {
+        user_id: 2,
+        sent_user_id: 1,
+        message: "user2dayo"
+    }
+])
+
+Tweet.create!([
+    {
+        user_id: 1,
+        tweet: "tweet1"
+    }, {
+        user_id: 1,
+        tweet: "tweet2"
+    }, {
+        user_id: 2,
+        tweet: "tweet1"
+    }
+])
