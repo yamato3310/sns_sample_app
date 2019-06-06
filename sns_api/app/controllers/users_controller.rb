9624d98f98dc3f3a2524fa19bd83ded6af0f7861
yamato0331 <yamatotunasima1@gmail.com>
2019-06-07 08:58:56 +0900

    jsonの返却

--- sns_api/app/controllers/users_controller.rb
+++ sns_api/app/controllers/users_controller.rb
@@ -1,4 +1,14 @@
 class UsersController < ApplicationController
   def show
+    render json: User.all
+  end
+
+  def find
+    render json: User.find(params[:id])
+  end
+
+  def shwo_tweets
+    render json: User.joins(:tweets).select('users.*, tweets.*').all()
+    # render json: Tweet.all()
   end
 end
--- sns_api/config/routes.rb
+++ sns_api/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/show'
+ get '/', to: 'users#show'
+ get '/user/:id', to: 'users#find'
+ get '/user_tweet', to: 'users#shwo_tweets'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
