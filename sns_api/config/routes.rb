063b697c28407326dd84612a1d0fa2509cc5326e
yamato0331 <yamatotunasima1@gmail.com>
2019-06-20 13:02:50 +0900

    only追加

--- sns_api/config/routes.rb
+++ sns_api/config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root 'users#index'
 
   resources :users do 
-    resources :tweets, module: :users
+    resources :tweets, module: :users, only: [index, show]
   end
 end
