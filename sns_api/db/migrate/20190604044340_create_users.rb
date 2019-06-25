de7c050bf5ced2bb7aaa363ceba7f8d7b7b09f5c
yamato0331 <yamatotunasima1@gmail.com>
2019-06-25 11:38:23 +0900

    user.passwordを削除

--- sns_api/db/migrate/20190604044340_create_users.rb
+++ sns_api/db/migrate/20190604044340_create_users.rb
@@ -2,7 +2,6 @@ class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users, id: :string do |t|
       t.string :name
-      t.text :password
       t.string :email
       t.timestamps
     end
--- sns_api/db/schema.rb
+++ sns_api/db/schema.rb
@@ -49,7 +49,7 @@ ActiveRecord::Schema.define(version: 20190625021356) do
 
   create_table "users", id: :string, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name"
-    t.text "password"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
