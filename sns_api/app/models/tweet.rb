fb2f989bb0ca693a6ba7ada735dfdc4bea989f67
yamato3310 <yamato3310.tuna@gmail.com>
2019-07-01 17:59:20 +0900

    tweets#index追加

--- sns_api/app/models/tweet.rb
+++ sns_api/app/models/tweet.rb
@@ -1,3 +1,12 @@
 class Tweet < ApplicationRecord
     has_one :users
+
+    def index
+        begin
+            tweets = Tweet.where!(user_id: params[user_id])
+            render status: 200, json: tweets
+        rescue => error
+            render status: 400, json: error
+        end
+    end
 end
