e6d336c44e2b5f463790e1996b83cebbcdc8f527
yamato3310 <yamato3310.tuna@gmail.com>
2019-07-01 19:39:19 +0900

    POST  /users/:user_id/tweets

--- sns_api/app/models/tweet.rb
+++ sns_api/app/models/tweet.rb
@@ -1,12 +1,5 @@
 class Tweet < ApplicationRecord
     has_one :users
 
-    def index
-        begin
-            tweets = Tweet.where!(user_id: params[user_id])
-            render status: 200, json: tweets
-        rescue => error
-            render status: 400, json: error
-        end
-    end
+    validates :tweet, presence: true, length: { maximum: 100 }
 end
