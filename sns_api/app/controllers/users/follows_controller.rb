e82a3d25223ffdeb6c4a4e03bd26f192b8fc309d
yamato3310 <yamato3310.tuna@gmail.com>
2019-07-01 22:48:27 +0900

    GET   /users/:user_id/follows

--- sns_api/app/controllers/users/follows_controller.rb
+++ sns_api/app/controllers/users/follows_controller.rb
@@ -1,5 +1,10 @@
 class Users::FollowsController < ApplicationController
     def index
-        render json: Follow.where(followed_id: params[:user_id])
+        begin
+            users = User.joins(:follows).where(follows: { followed_id: params[:user_id] })
+            render status: 200, json: { result: "OK", users: users } 
+        rescue => error
+            render status: 400, json: { result: "NG", error: error }
+        end
     end
 end
--- sns_api/app/models/user.rb
+++ sns_api/app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
     has_many :tweets
+    has_many :follows
+
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
     validates :name, presence: true
