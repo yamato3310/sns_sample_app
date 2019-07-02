f629d62e7f2ab5296ccb1b361f9433f42c157124
yamato3310 <yamato3310.tuna@gmail.com>
2019-07-03 00:32:04 +0900

    pgに移行

--- sns_api/db/schema.rb
+++ sns_api/db/schema.rb
@@ -12,50 +12,46 @@
 
 ActiveRecord::Schema.define(version: 20190626002041) do
 
-  create_table "follows", id: :string, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "follows", id: :string, force: :cascade do |t|
     t.string "user_id"
     t.string "followed_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["followed_id"], name: "fk_rails_5ef72a3867"
-    t.index ["user_id"], name: "fk_rails_32479bd030"
   end
 
-  create_table "messages", id: :string, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "messages", id: :string, force: :cascade do |t|
     t.string "user_id"
     t.string "sent_user_id"
     t.text "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["sent_user_id"], name: "fk_rails_3be5ebccac"
-    t.index ["user_id"], name: "fk_rails_273a25a7a6"
   end
 
-  create_table "passwords", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "passwords", force: :cascade do |t|
     t.string "user_id"
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "fk_rails_cac37af949"
   end
 
-  create_table "sessions", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "sessions", force: :cascade do |t|
     t.string "user_id"
     t.string "token"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "fk_rails_758836b4f0"
   end
 
-  create_table "tweets", id: :string, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "tweets", id: :string, force: :cascade do |t|
     t.string "user_id"
     t.text "tweet"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "fk_rails_003928b7f5"
   end
 
-  create_table "users", id: :string, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "users", id: :string, force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
